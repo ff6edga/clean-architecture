dc27130b62b56a3e90dd4ab0d7680eb7cd80941c
younsoo.kim <younsoo.kim@meshkorea.net>
2023-06-07 15:17:58 +0900

    계층으로 구성하기

---
+++ src/main/java/io/reflectoring/buckpal/domain/AccountRepository.java
@@ -0,0 +1,5 @@
+package io.reflectoring.buckpal.domain;
+
+public class AccountRepository {
+
+}
---
+++ src/main/java/io/reflectoring/buckpal/persistence/AccountRepositoryImpl.java
@@ -0,0 +1,5 @@
+package io.reflectoring.buckpal.persistence;
+
+public class AccountRepositoryImpl {
+
+}
